4a17ac38866dcaf71a3ce8f4bdc3e3f803b18970
dejaa1 <70174295+dejaa1@users.noreply.github.com>
2024-02-19 22:56:52 -0500

    rename

--- ccxt_get_exchanges.py
+++ ccxt_get_exchanges.py
@@ -1,8 +1,12 @@
 import ccxt
 import pandas as pd
 
+### This block of code gets exchange info from kraken
+### This needs to be repeated for the exchanges we chose
+
 kraken = ccxt.kraken()
 kraken_json = (kraken.fetch_tickers())
 kraken_df = pd.DataFrame.from_dict(kraken_json, orient='index')
 kraken_df.to_csv("CAT/exchanges/kraken_df")
 print(kraken_df.index)
+
