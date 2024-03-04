4e606d2440ce9260eac59f36d5c49d44c74e66ee
dejaa1 <70174295+dejaa1@users.noreply.github.com>
2024-03-04 15:33:30 -0500

    h

--- get_common_pairs.py
+++ get_common_pairs.py
@@ -10,12 +10,13 @@ exchange_save_path = "CAT/exchanges_intersection/"
 def find_intersection(exchange_combo):
     for exchange_group in exchange_combo:
         df_1 = pd.read_csv(exchange_path + exchange_group[0] + "_df.csv")
-        intersection_set = set(list(df_1['symbol']))
+        symbols1 = list(df_1['symbol'])     
+        intersection_set = set(list(symbols1))
         table_name = '_'.join(exchange_group)
         for exchange in list(exchange_group):
             df = pd.read_csv(exchange_path + exchange + "_df.csv")
-            symbols = list(df['symbol'])
-            intersection_set = intersection_set.intersection(list(symbols))
+            symbols_local = list(df['symbol'])
+            intersection_set = intersection_set.intersection(list(symbols_local))
         
         df_to_save = pd.DataFrame(intersection_set, columns=['symbol'])
         df_to_save.to_csv(exchange_save_path + table_name + "_intersection.csv")
